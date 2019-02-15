daa4f5a91a7871f15ad11718c73717d2f4923e8e
Jamie Webber <jamiecwebber@gmail.com>
2019-02-15 12:13:06 -0500

    deploying to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [ :index, :new, :show, :create ] do
+    resources :doses, only: [ :index, :new, :create]
+  end
+  get "/", to:"cocktails#index"
+
+  resources :doses, only: :destroy
 end
